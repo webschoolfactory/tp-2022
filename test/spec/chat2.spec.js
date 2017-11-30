9cd8b09657a3796b2c1b215b7ca044434cbc62d2
Loïc Calvy <lcalvy@gmail.com>
2017-11-30 21:26:29 +0100

    add sample test to implement

---
+++ test/spec/chat2.spec.js
@@ -0,0 +1,30 @@
+// const assert = require('assert');
+// const chat = require('../../chat');
+
+describe('chat', function() {
+  it('should show typing', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should add new user', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should not add user', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should stop typing', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should show disconnect', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should show not disconnect', function(done) {
+    done(new Error('not tested'));
+  });
+});
+
+
