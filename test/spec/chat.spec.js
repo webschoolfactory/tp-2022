3d9d329471ecdaba880c975bc7619e86e974a00b
Loïc Calvy <lcalvy@gmail.com>
2017-12-01 12:22:06 +0100

    empty tests

--- test/spec/chat.spec.js
+++ test/spec/chat.spec.js
@@ -19,6 +19,30 @@ describe('chat', function() {
     const newMessage = chat.newMessage(socket);
     newMessage('yolo');
   });
+
+  it('should show typing', function(done) {
+      done(new Error('not tested'));
+  });
+
+  it('should add new user', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should not add user', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should stop typing', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should show disconnect', function(done) {
+    done(new Error('not tested'));
+  });
+
+  it('should show not disconnect', function(done) {
+    done(new Error('not tested'));
+  });
 });
 
 
